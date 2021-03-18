fb4cf5c77371b4ff042ff7131a786d543c18c0aa
robj <robert.jennings@gmail.com>
2021-03-18 17:00:35 +1030

    bucket_url -> bucket_name

--- docker/casper-kube-node/init.sh
+++ docker/casper-kube-node/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bucket_url=http://builds.casperlabs.io
+bucket_name="builds.casperlabs.io"
 
 
 if [ -z "$NETWORK_NAME" ]
@@ -18,10 +18,10 @@ fi
 
 
 #config
-aws s3 sync s3://$bucket_url/networks/$NETWORK_NAME/nodes/casper-node-$CASPER_NODE_INDEX/etc/ /etc/
+aws s3 sync s3://$bucket_name/networks/$NETWORK_NAME/nodes/casper-node-$CASPER_NODE_INDEX/etc/ /etc/
 
 #binary
-aws s3 sync s3://$bucket_url/networks/$NETWORK_NAME/staging/bin /var/lib/casper/bin
+aws s3 sync s3://$bucket_name/networks/$NETWORK_NAME/staging/bin /var/lib/casper/bin
 chmod +x /var/lib/casper/bin/1_0_0/casper-node
 
 
