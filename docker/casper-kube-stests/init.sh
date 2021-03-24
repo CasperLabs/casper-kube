d679f3ba8427bcf42a240541ea865827619a7641
robj <robert.jennings@gmail.com>
2021-03-24 21:18:55 +1030

    s3 sync stests network

--- docker/casper-kube-stests/init.sh
+++ docker/casper-kube-stests/init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#stests network
+#https://github.com/CasperLabs/stests/blob/master/docs/usage_lrt.md
+
+aws s3 sync s3://$bucket_name/networks/$NETWORK_NAME/stests_net/ /root/.casperlabs-stests/nets/kubenet/
+
 service redis-server start 
-
-
-
 tail -f /dev/null
