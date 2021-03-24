341bd31c1d328c362e29838b6038ae8449a15b92
robj <robert.jennings@gmail.com>
2021-03-24 21:25:11 +1030

    bucket_name missing

--- docker/casper-kube-stests/init.sh
+++ docker/casper-kube-stests/init.sh
@@ -3,6 +3,7 @@
 #stests network
 #https://github.com/CasperLabs/stests/blob/master/docs/usage_lrt.md
 
+bucket_name="builds.casperlabs.io"
 aws s3 sync s3://$bucket_name/networks/$NETWORK_NAME/stests_net/ /root/.casperlabs-stests/nets/kubenet/
 
 service redis-server start 
