bfc56001cab0db185703eb792914631366a7d643
robj <robert.jennings@gmail.com>
2021-03-28 14:01:41 +1100

    ["consensus"]["highway"]["unit_hashes_folder"] -> ["consensus"]["unit_hashes_folder"]

--- casper-tool.py
+++ casper-tool.py
@@ -442,7 +442,7 @@ def generate_node(known_addresses, obj, nodes_path, node_version, public_address
     storage_path = "/storage/{}".format(public_address)
     config["storage"]["path"] = storage_path
     config["network"]["gossip_interval"] = 120000
-    config["consensus"]["highway"]["unit_hashes_folder"] = storage_path
+    config["consensus"]["unit_hashes_folder"] = storage_path
     # config["consensus"]["unit_hashes_folder"] = storage_path
     toml.dump(config, open(os.path.join(node_config_path, "config.toml", ), "w"))
 
